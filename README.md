4874f1adf6340da158638f48988228c4b99c6643
pedr <pedrlz.frn@gmail.com>
2021-11-14 09:42:32 -0300

    GeneratePrimes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # TDD - Jason Gorman
 
-Exercises written in C# with XUnit.Net from the excelent and free (!!) book about TDD.
+Exercises written in C# with xUnit.net from the excelent and free (!!) book about TDD.
 
 Thanks for Jason Gorman and Codemanship for this wonderful resource.
 http://www.codemanship.co.uk/tdd_jasongorman_codemanship.pdf
--- Tests/Chapter4/GeneratePrimesTest.cs
+++ Tests/Chapter4/GeneratePrimesTest.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using Base.Chapter4;
 using Xunit;
 
 namespace Tests.Chapter4
@@ -7,13 +10,90 @@ namespace Tests.Chapter4
         Test-drive some code that will generate a list of prime
         numbers that are less than 1,000
     */
-
     public class GeneratePrimesTest
     {
-        [Fact]
-        public void Test1()
+        [Theory, MemberData(nameof(all_numbers_until_3_are_primes_data))]
+        public void all_numbers_until_3_are_primes(int until, List<int> expected)
         {
+            var primes = GeneratePrimes.Until(until);
 
+            Assert.Equal(expected, primes);
+        }
+
+        public static IEnumerable<object[]> all_numbers_until_3_are_primes_data
+        {
+            get
+            {
+                // Or this could read from a file. :)
+                return new[]
+                {
+                    new object[] { 2 , new List<int> {  2 } },
+                    new object[] { 3 , new List<int> {  2, 3 } },
+                };
+            }
+        }
+
+
+        [Theory, MemberData(nameof(multiple_of_two_bigger_than_two_are_not_primes_data))]
+        public void multiple_of_two_bigger_than_two_are_not_primes(int until, List<int> expected)
+        {
+            var primes = GeneratePrimes.Until(until);
+
+            Assert.Equal(expected, primes);
+        }
+
+        public static IEnumerable<object[]> multiple_of_two_bigger_than_two_are_not_primes_data
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { 4, new List<int> {  2, 3 } },
+                    new object[] { 6, new List<int> {  2, 3, 5 } },
+                    new object[] { 8, new List<int> {  2, 3, 5, 7 } },
+                };
+            }
+        }
+
+        [Theory, MemberData(nameof(all_odd_numbers_are_prime_data))]
+        public void all_odd_numbers_are_prime(int until, List<int> expected)
+        {
+            var primes = GeneratePrimes.Until(until);
+
+            Assert.Equal(expected, primes);
+        }
+
+        public static IEnumerable<object[]> all_odd_numbers_are_prime_data
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { 3, new List<int> {  2, 3 } },
+                    new object[] { 5, new List<int> {  2, 3, 5 } },
+                    new object[] { 7, new List<int> {  2, 3, 5, 7 } },
+                };
+            }
+        }
+
+        [Theory, MemberData(nameof(all_numbers_divisible_by_primes_are_not_primes_data))]
+        public void all_numbers_divisible_by_primes_are_not_primes(int until, List<int> expected)
+        {
+            var primes = GeneratePrimes.Until(until);
+
+            Assert.Equal(expected, primes);
+        }
+        public static IEnumerable<object[]> all_numbers_divisible_by_primes_are_not_primes_data
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { 9, new List<int> {  2, 3, 5, 7 } },
+                    new object[] { 15, new List<int> {  2, 3, 5, 7, 11, 13 } },
+                    new object[] { 19, new List<int> {  2, 3, 5, 7, 11, 13, 17, 19 } },
+                };
+            }
         }
     }
 }
